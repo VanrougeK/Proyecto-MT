37745373b666cca7b7efc79452754acbe27d1354
VanrougeK <ochoavania795@gmail.com>
2026-03-02 19:37:46 -0600

    Importes de componentes a App.jsx

--- MT_PROYECT/src/App.jsx
+++ MT_PROYECT/src/App.jsx
@@ -2,6 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Servicios from './components/Servicios'
+import Opiniones from './components/Opiniones'
+import Contacto from './components/Contacto'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
