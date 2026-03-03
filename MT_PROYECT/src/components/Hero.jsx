cede4cc516d8f3968922902503b54aff7b66383a
VanrougeK <ochoavania795@gmail.com>
2026-03-02 20:10:07 -0600

    Moficiaciones de Hero.jsx y App.jsx
    Se importo todo de los componentes al App.jsx y se empezo a modificar el Hero.jsx. Falta agregar diseño al Navbar.jsx

---
+++ MT_PROYECT/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+    const stats = ["+100", "Equipos reparados" , "1500%", "Clientes satisfechos" , "24hrs", "Diagnostico rapido"]
+    const benefits = ["Sin cobro por diagnostico", "Garantia en todas las reparaciones", "Presupuesto sin compromiso"]
+
+    return (
+        
+    )
+}
--- MT_PROYECT/src/components/Navbar.jsx
+++ MT_PROYECT/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 export default function Navbar() {
+    const [open, setOpen] = useState(false)
   const links = ["Inicio", "Servicios", "Opiniones", "Contacto"];
-}
 
 return (
-  <nav>
+  <nav className="fixed top-0 left-0 right-0 z-100 bg-white/92 backdrop-blur-md border-b-2 border-[#f0e8d8] font-sans">
     <div>
       {/* logo jiji */}
       <div>ᯓ★</div>
@@ -13,25 +13,36 @@ return (
         <span>ElectroPro</span>
         <span>Reparaciones</span>
       </div>
-    
 
-    {/* Links */}
-    <ul className="nav-links">
-        <li>
-            <a href=""></a>
-        </li>
-    </ul>
+      {/* Links */}
+      <ul className="nav-links">
+        {links.map((link) => (
+          <li>
+            <a href={`#${link.toLowerCase()}`}> {link} </a>
+          </li>
+        ))}
+      </ul>
 
-    <a href="#Contacto"></a>
+      <a href="#Contacto">Cotizar</a>
 
-    <button className="burger" onClick={() => SecurityPolicyViolationEvent(!open)}> ☰ </button>
+      <button
+        className="burger"
+        onClick={() => SecurityPolicyViolationEvent(!open)}
+      >☰</button>
     </div>
 
     {/* Menu en el cel */}
     {open && (
-        <div>
-            {links.map(link=> ( <a key={link} href={`#${link.toLowerCase()}`} onClick={() => StereoPannerNode(false)}></a>))}
-        </div>
+      <div>
+        {links.map((link) => (
+          <a
+            key={link}
+            href={`#${link.toLowerCase()}`}
+            onClick={() => StereoPannerNode(false)}
+          >{link}</a>
+        ))}
+      </div>
     )}
   </nav>
 );
+}
