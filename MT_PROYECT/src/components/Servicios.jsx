b7beb213f5f388ec847c596f66c727828a90bad5
VanrougeK <ochoavania795@gmail.com>
2026-03-07 22:27:31 -0600

    Modificacion a servicios.jsx

--- MT_PROYECT/src/components/Servicios.jsx
+++ MT_PROYECT/src/components/Servicios.jsx
@@ -1,1 +1,21 @@
 import import { useState } from "react";
+
+export default function Servicios() {
+    return(
+        <section>
+            
+            <div>
+                <div>
+                <h2>Servicios</h2>
+                <p>Dibujos jsjjs agregar mas en formato lista</p>
+            </div>
+
+            <div>
+                <h2>Reglas</h2>
+                <h3>¿Que hago y que <b>no</b> hago?</h3>
+            </div>
+            </div>
+            
+        </section>
+    )
+}
